2159a027e79063eae62213b1a7e439cbec7f8eb8
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-02-29 08:18:29 +0900

    added morgan logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
 
@@ -25,6 +26,17 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+morgan.token("my-data", (request, response) => {
+  const body = request.body;
+  return JSON.stringify(body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :my-data"
+  )
+);
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello</h1>");
